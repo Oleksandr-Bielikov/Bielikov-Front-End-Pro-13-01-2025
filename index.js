465540669168c698569a131e21560654e1972fa3
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-02-07 20:39:10 +0200

    Add HW 8

--- index.js
+++ index.js
@@ -1,94 +1,101 @@
-/*
-const userAgeOfBirth = prompt('Вкажіть Ваш рік народження.');
+///
+let i = 10;
+let result = "";
 
-if (userAgeOfBirth == null) {
-    alert("Шкода, що Ви не захотіли ввести свій рік народження.");
-} else {
-    const userCity = prompt('Вкажіть місто в якому Ви проживаєте.');
-if (userCity == null) {
-    alert("Шкода, що Ви не захотіли ввести своє місто.");
-} else {
-    const userSport = prompt('Вкажіть Ваш улюблений вид спорту');
-if (userSport == null) {
-    alert("Шкода, що Ви не захотіли ввести свій вид спорту.");
-  } else {
-    let cityMessage;
-if (userCity === "Київ") {
-    cityMessage = "Ти живеш в столиці України";
-} else if (userCity === "Вашингтон") {
-    cityMessage = "Ти живеш в столиці США";
-} else if (userCity === "Лондон") {
-    cityMessage = "Ти живеш в столиці Великої Британії";
-} else {
-    cityMessage = `Ти живеш в місті ${userCity}`; 
-} 
-    let sportMessage;
-if (userSport === "Бокс") {
-    sportMessage = "Круто! Хочеш стати Майком Тайсоном";
-} else if (userSport === "Футбол") {
-    sportMessage = "Круто! Хочеш стати Ліонелем Мессі";
-} else if (userSport === "Баскетбол") {
-    sportMessage = "Круто! Хочеш стати Майклом Джорданом";
-} else {
-    sportMessage = `Круто! ${userSport} це чудовий вид спорту!`;
-}
-alert(`Ти ${userAgeOfBirth} року народження\n${cityMessage}\n${sportMessage}`);
-}
-  }
-}
-*/
-const userAgeOfBirth = prompt('Вкажіть Ваш рік народження.');
-
-if (userAgeOfBirth == null) {
-    alert("Шкода, що Ви не захотіли ввести свій рік народження.");
-} else {
-    const userCity = prompt('Вкажіть місто в якому Ви проживаєте.');
-
-    if (userCity == null) {
-        alert("Шкода, що Ви не захотіли ввести своє місто.");
-    } else {
-        const userSport = prompt('Вкажіть Ваш улюблений вид спорту');
-
-    if (userSport == null) {
-            alert("Шкода, що Ви не захотіли ввести свій вид спорту.");
-    } else {
-        let cityMessage;
-        switch (userCity) {
-            case "Київ":
-            cityMessage = "Ти живеш в столиці України";
-            break;
-            
-            case "Вашингтон":
-            cityMessage = "Ти живеш в столиці США";
-            break;
-
-            case "Лондон":
-            cityMessage = "Ти живеш в столиці Великої Британії";
-            break;
-
-            default:
-            cityMessage = `Ти живеш в місті ${userCity}`;
-            }
-
-        let sportMessage;
-        switch (userSport) {
-            case "Бокс":
-            sportMessage = "Круто! Хочеш стати Майком Тайсоном";
-            break;
-
-            case "Футбол":
-            sportMessage = "Круто! Хочеш стати Ліонелем Мессі";
-            break;
-
-            case "Баскетбол":
-            sportMessage = "Круто! Хочеш стати Майклом Джорданом";
-            break;
-
-            default:
-            sportMessage = `Круто! ${userSport} це чудовий вид спорту!`;
-            }
-
-        alert(`Ти ${userAgeOfBirth} року народження\n${cityMessage}\n${sportMessage}`);
-        }
+while (i <= 20) {
+    result += i;
+    if (i < 20) {
+        result += ", ";
     }
+    i++;
+}
+alert(result);
+
+///
+result = "";
+for(let i = 10; i <= 20; i++) {
+    result += i * i + ", ";
+}
+alert(result);
+
+///
+result = 0;
+for(let i = 0; i <= 15; i++) {
+    result += i;
+}
+alert(result);
+
+///
+result = 1;
+for(let i = 15; i <= 35; i++) {
+    result *= i;
+}
+alert(result);
+
+///
+result = 0;
+for(let i = 1; i <= 500; i++) {
+    result += i;
+}
+alert(result / 500);
+
+///
+result = 0;
+for(let i = 30; i <= 80; i++) {
+    if(i % 2 !== 0) {
+    continue
+    }
+    result += i;
+}
+alert(result);
+
+///
+result = "";
+for(let i = 100; i <= 200; i += 3) {
+    result += i;
+    if (i + 3 < 200) {
+        result += ", ";
+    }
+}
+alert(result);
+
+///
+let numb = Number(prompt("Введіть число!"));
+
+result = "";
+for(let i = 1; i <= numb; i++) {
+    if(numb % i === 0) {
+        result += i + " ";
+    }
+}
+alert(result);
+
+///
+let numb_1 = Number(prompt("Введіть число!"));
+
+result = "";
+for(let i = 1; i <= numb_1; i++) {
+    if(numb_1 % i === 0 && i % 2 === 0) {
+        result += i + " ";
+    }
+}
+alert(result);
+
+///
+let numb_2 = Number(prompt("Введіть число!"));
+
+result = 0;
+for(let i = 1; i <= numb_2; i++) {
+    if(numb_2 % i === 0 && i % 2 === 0) {
+        result += i;
+    }
+}
+alert(result);
+
+///
+for(i = 1; i <= 10; i++) {
+    for(j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+
 }
