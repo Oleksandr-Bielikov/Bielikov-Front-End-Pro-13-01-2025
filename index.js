5846112a6a411822e8cac6612e9d8b97f602ec99
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-03-02 19:28:07 +0200

    Add new HW 14

--- index.js
+++ index.js
@@ -13,16 +13,18 @@ console.log(sum(5));
 console.log(sum(20));
 
 // Даний масив з елементами різних типів. Створити функцію, яка вираховує середнє арифметичне лише числових елементів даного масиву.
-let arr = [1, 2, 'qwerty', 3, 'John', 4, 'apple', 5];
+let arr = [1, 2, 'qwerty', 3, 'John', 4, 'apple', 5, NaN];
 
 function averageOfNum(arr) {
-    let sum = 0;
+  let sum = 0;
+  let count = 0;
     for (let i of arr) {
-        if (typeof i == 'number') {
-            sum += i;
+        if (typeof i == 'number' && !isNaN(i)) {
+          sum += i;
+          count++;
         }
     }
-    return(sum);
+  return count > 0 ? sum / count : 0;
 }
 console.log(averageOfNum(arr));
 
