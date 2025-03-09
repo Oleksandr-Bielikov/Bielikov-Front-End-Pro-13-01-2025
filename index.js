cc1c5563285147d9b28dd51b7e117c8edc10e0ab
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-03-09 22:26:10 +0200

    Add HW 16

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-// Реалізувати рекурсивну функцію, яка зводить число в ступінь.
-// Число, яке потрібно звести в ступінь, передається як перший аргумент у функцію
-// Ступінь передається як другий аргумент у функцію
-// pow(num, degree)
+// Вам потрібно написати функцію, яка як параметр приймає функцію і додає їй можливість кешувати дзвінки. 
+// Ідея полягає в тому, що при виклику функції з однаковими аргументами немає сенсу викликати функцію щоразу, 
+// достатньо зберігати дані про результати виклику.
+// Зберігати потрібно останні 10 дзвінків.
 
-function pow(num, degree) {
-  return (degree == 1) ? num : (num * pow(num, degree - 1));
+function cachePhones() {
+    let cache = [];
+
+    return function (x) {
+        if (!cache.includes(x)) {
+            cache.push(x);
+        }
+
+        if (cache.length > 10) {
+            cache.shift()
+        }
+
+        return cache;
+    };
 }
-console.log(pow(2, 3));
+let cache = cachePhones();
+
+console.log(cache(1));
+console.log(cache(2));
+console.log(cache(1));
+console.log(cache(3));
+console.log(cache(1));
+console.log(cache(4));
+console.log(cache(1));
+console.log(cache(5));
+console.log(cache(1));
+console.log(cache(6));
+console.log(cache(1));
+console.log(cache(7));
+console.log(cache(1));
+console.log(cache(8));
+console.log(cache(1));
+console.log(cache(9));
+console.log(cache(1));
+console.log(cache(10));
+console.log(cache(1));
+console.log(cache(11));
+console.log(cache(1));
+console.log(cache(12));
+
