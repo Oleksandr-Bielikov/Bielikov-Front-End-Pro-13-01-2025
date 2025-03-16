8f951ed5de083dcd5606b698e4902a89eaf5ce79
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-03-16 17:31:11 +0200

    Add HW 18

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-// Вам потрібно написати функцію, яка як параметр приймає функцію і додає їй можливість кешувати дзвінки. 
-// Ідея полягає в тому, що при виклику функції з однаковими аргументами немає сенсу викликати функцію щоразу, 
-// достатньо зберігати дані про результати виклику.
-// Зберігати потрібно останні 10 дзвінків.
+// The ladder (сходи) – об'єкт, який дозволяє підійматися вгору та спускатися:
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показує теперішню сходинку
+        alert(this.step);
+        return this;
+    }
+};
 
-function cachePhones() {
-    let cache = [];
+// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
 
-    return function (x) {
-        if (!cache.includes(x)) {
-            cache.push(x);
-        }
-
-        if (cache.length > 10) {
-            cache.shift()
-        }
-
-        return cache;
-    };
-}
-let cache = cachePhones();
-
-console.log(cache(1));
-console.log(cache(2));
-console.log(cache(1));
-console.log(cache(3));
-console.log(cache(1));
-console.log(cache(4));
-console.log(cache(1));
-console.log(cache(5));
-console.log(cache(1));
-console.log(cache(6));
-console.log(cache(1));
-console.log(cache(7));
-console.log(cache(1));
-console.log(cache(8));
-console.log(cache(1));
-console.log(cache(9));
-console.log(cache(1));
-console.log(cache(10));
-console.log(cache(1));
-console.log(cache(11));
-console.log(cache(1));
-console.log(cache(12));
-
+// Змініть код методів up, down і showStep таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
+ladder.up().up().down().showStep(); // 1
+// Такий підхід широко використовується в бібліотеках JavaScript.
