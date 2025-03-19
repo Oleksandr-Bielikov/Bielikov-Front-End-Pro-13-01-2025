82321762ca50c6a0fccfe128a03d8d64d95eba49
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-03-19 16:14:58 +0200

    Add HW 20

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
-let slider = document.querySelector('.images');
-let prevButton = document.querySelector('#prev');
-let nextButton = document.querySelector('#next');
-let slides = slider.querySelectorAll('img');
-let slideIndex = 0;
+let buttons = document.querySelectorAll('.button');
+let counter = document.querySelectorAll('.counter');
 
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
 
-function updateSlider() {
-  for (let i = 0; i < slides.length; i++) {
-    if (i === slideIndex) {
-      slides[i].style.display = 'block';
-    } else {
-      slides[i].style.display = 'none';
-    }
-  }
-
-  if (slideIndex === 0) {
-    prevButton.disabled = true;
-  } else {
-    prevButton.disabled = false;
-  }
-
-  if (slideIndex === slides.length - 1) {
-    nextButton.disabled = true;
-  } else {
-    nextButton.disabled = false;
-  }
-}
-
-function showPreviousSlide() {
-  if (slideIndex > 0) {
-    slideIndex--;
-    updateSlider();
-  }
-}
-
-function showNextSlide() {
-  if (slideIndex < slides.length - 1) {
-    slideIndex++;
-    updateSlider();
-  }
-}
-
-updateSlider();
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        let count = parseInt(counter[i].textContent);
+        counter[i].textContent = count + 1;
+    });
+};
