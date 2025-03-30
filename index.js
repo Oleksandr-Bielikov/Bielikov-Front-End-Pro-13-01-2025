aaaaf250e135520a9682f48def28d8473445ac9e
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-03-30 13:05:04 +0300

    Add HW 24

--- index.js
+++ index.js
@@ -1,84 +1,35 @@
-// Реалізувати подобу інтернет-магазину
+const form = document.getElementById('form');
+const formContainer = document.getElementById('form-container');
+const table = document.getElementById('table');
 
-// Дано 3 блоки
+const languages = document.querySelectorAll('input[name="language"]');
 
-// У лівій частині сторінки – перелік категорій.
-// При натисканні на категорію виводиться у середній блок список товарів цієї категорії.
-// Клік на товар – інформацію про товар у правому блоці.
-// В інформації товару - кнопка "купити"
-// При натисканні на “купити” з'являється повідомлення, що товар куплено та повернення у вихідний стан програми (коли відображається лише список категорій)
 
-let item = document.getElementsByTagName('p');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-for (let i of item) {
-    i.addEventListener('click', function () {
-        showList(i.textContent);
+    let name = form.elements['name'].value;
+    let surname = form.elements['surname'].value;
+    let date = form.elements['date'].value;
+    let gender = form.elements['gender'].value;
+    let city = form.elements['city'].value;
+    let adress = form.elements['adress'].value;
+
+    let selectedLanguages = [];
+    languages.forEach((box) => {
+      if (box.checked) {
+        selectedLanguages.push(box.value);
+      }
     });
-}
 
-let avtoList = document.querySelector('.avto');
-let motoList = document.querySelector('.moto');
+    document.getElementById('res-name').textContent = name;
+    document.getElementById('res-surname').textContent = surname;
+    document.getElementById('res-date').textContent = date;
+    document.getElementById('res-gender').textContent = gender;
+    document.getElementById('res-city').textContent = city;
+    document.getElementById('res-adress').textContent = adress;
+    document.getElementById('res-languages').textContent = selectedLanguages.join(', ');
 
-function hiddenList() {
-    avtoList.style.display = 'none';
-    motoList.style.display = 'none';
-}
-
-function showList(item) {
-    hiddenList();
-    if (item === 'AVTO') {
-        avtoList.style.display = 'block';
-    } else if (item === 'MOTO') {
-        motoList.style.display = 'block';
-    }
-}
-
-let avtoOne = document.querySelector('.avto-1');
-let avtoTwo = document.querySelector('.avto-2');
-let motoOne = document.querySelector('.moto-1');
-let motoTwo = document.querySelector('.moto-2');
-
-let avtoItems = avtoList.querySelectorAll('li');
-let motoItems = motoList.querySelectorAll('li');
-
-
-for (let i = 0; i < avtoItems.length; i++) {
-    avtoItems[i].addEventListener('click', function () {
-        showItem(avtoItems[i].textContent);
-    })
-}
-for (let i = 0; i < motoItems.length; i++) {
-    motoItems[i].addEventListener('click', function () {
-        showItem(motoItems[i].textContent);
-    })
-}
-
-function hiddenItems () {
-    avtoOne.style.display = 'none';
-    avtoTwo.style.display = 'none';
-    motoOne.style.display = 'none';
-    motoTwo.style.display = 'none';
-}
-
-function showItem (item) {
-    hiddenItems()
-    if (item === 'Avto 1') {
-        avtoOne.style.display = 'block';
-    } else if (item === 'Avto 2') {
-        avtoTwo.style.display = 'block';
-    } else if (item === 'Moto 1') {
-        motoOne.style.display = 'block';
-    } else if (item === 'Moto 2') {
-        motoTwo.style.display = 'block';
-    }
-}
-
-let button = document.getElementsByClassName('button');
-
-for (let i of button) {
-    i.addEventListener('click', function () {
-        alert('Товар куплено!');
-        hiddenItems();
-        hiddenList();
-    })
-}
+    formContainer.style.display = 'none';
+    table.style.display = 'block';
+});
