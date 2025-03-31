d307fba06841607ca0ce8e0cb7af7642ecc89173
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-03-31 10:17:45 +0300

    Add HW 25

--- index.js
+++ index.js
@@ -77,8 +77,66 @@ let button = document.getElementsByClassName('button');
 
 for (let i of button) {
     i.addEventListener('click', function () {
-        alert('Товар куплено!');
-        hiddenItems();
-        hiddenList();
+        formContainer.style.display = 'block';
     })
 }
+
+/// Доповнення інтернет-магазину ДЗ 25
+const form = document.getElementById('form');
+const formContainer = document.getElementById('form-container');
+const table = document.getElementById('table');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let name = form.elements['name'].value;
+    let surname = form.elements['surname'].value;
+    let secondName = form.elements['second-name'].value;
+    let city = form.elements['city'];
+    let post = form.elements['post'].value;
+    let pay = form.elements['pay'].value;
+    let quantity = form.elements['quantity'].value;
+    let comment = form.elements['comment'].value;
+
+    const namePattern = /^[А-ЯІЇЄҐа-яіїєґ'’\-]+$/u;
+
+    if (!namePattern.test(name) || !namePattern.test(surname) || !namePattern.test(secondName)) {
+    alert("Ім'я, прізвище та по-батькові повинні містити тільки літери!");
+    return;
+    };
+    document.getElementById('res-name').textContent = name;
+    document.getElementById('res-surname').textContent = surname;
+    document.getElementById('res-second-name').textContent = secondName;
+
+    if (city.selectedIndex === 0) {
+        alert("Оберіть місто!")
+        return;
+    };
+    document.getElementById('res-city').textContent = city.value;
+    
+    if (!/^\d+$/.test(post)) {
+    alert("Номер відділення пошти має містити тільки цифри!");
+    return;
+    };
+    document.getElementById('res-post').textContent = post;
+
+    document.getElementById('res-pay').textContent = pay;
+
+    if (isNaN(quantity) || quantity <= 0) {
+        alert("Введіть коректну кількість товару");
+        return;
+    };
+    document.getElementById('res-quantity').textContent = quantity;
+
+    if (comment.length > 500) {
+        alert("Коментар не може перевищувати 500 символів");
+        return;
+    };
+    document.getElementById('res-comment').textContent = comment;
+
+    table.style.display = 'block';
+    form.style.display = 'none';
+
+    hiddenItems();
+    hiddenList();
+})
