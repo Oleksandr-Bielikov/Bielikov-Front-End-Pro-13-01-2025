e5aa2829dcadb14bc46c95b167c9a1663f2eef7a
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-04-10 15:45:33 +0300

    Add HW_32

--- index.js
+++ index.js
@@ -1,49 +1,101 @@
-class Hamburger {
-    static SIZE_SMALL = { cost: 50, calories: 20 };
-    static SIZE_BIG = { cost: 100, calories: 40 };
+class Student {
 
-    static STUFFING_CHEESE = { cost: 10, calories: 20 };
-    static STUFFING_SALAD = { cost: 20, calories: 5 };
-    static STUFFING_POTATO = { cost: 15, calories: 10 };
+    constructor(name, surname, yearOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.marks = [];
+        this.logOfVisits = Array(25).fill(null);
+    };
 
-    static TOPPING_SAUCE = { cost: 15, calories: 0 };
-    static TOPPING_MAYO = { cost: 20, calories: 5 };
+    // Визначити вік студента
+    getAge() {
+        let year = new Date().getFullYear();
+        return `Студенту ${year - this.yearOfBirth} років`;
+    };
 
-    constructor(size, stuffing) {
-        this.size = size;
-        this.stuffing = stuffing;
-        this.toppings = [];
-    }
+    // Додати оцінку в масив оцінок
+    addMark(mark) {
+        let value = Number(mark);
+        this.marks.push(value);
+    };
 
-    addTopping(topping) {
-        this.toppings.push(topping);
-    }
+    // Визначити середнє значення оцінок
+    getAverageOfMarks() {
+       let resultOfMarks = this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+       return resultOfMarks;
+    };
 
-    calculatePrice() {
-        let totalPrice = this.size.cost + this.stuffing.cost;
-        for (let topping of this.toppings) {
-            totalPrice += topping.cost;
+    // Метод для почергового додавання значень в журнал відвідувань
+    #markVisit(value) {
+        let index = this.logOfVisits.indexOf(null);
+        if(index !== -1) {
+            this.logOfVisits[index] = value;
         }
-        return `${totalPrice} тугриків`;
-    }
+    };
 
-    calculateCalories() {
-        let totalCalories = this.size.calories + this.stuffing.calories;
-        for (let topping of this.toppings) {
-            totalCalories += topping.calories;
+    // Присутній
+    present() {
+        this.#markVisit(true);
+    };
+
+    // Відсутній
+    absent() {
+        this.#markVisit(false);
+    };
+
+    // Визначити середню відвідуваність
+    getAverageOfVisits () {
+        let visits = this.logOfVisits.filter(v => v !== null);
+        if (visits.length === 0) return 0;
+        let resultOfVisits = visits.filter(v => v === true).length / visits.length;
+        return resultOfVisits;
+    };
+    
+    summary () {
+        let averageMarks = this.getAverageOfMarks();
+        let averageVisits = this.getAverageOfVisits();
+
+        if (averageMarks > 90 && averageVisits > 0.9) {
+            return "Молодець!";
+        } else if (averageMarks < 90 && averageVisits < 0.9) {
+            return "Редиска!";
+        } else {
+            return "Добре, але можна краще!";
         }
-        return `${totalCalories} калорій`;
-    }
+    };
 }
-// // маленький гамбургер із начинкою із сиру
-// const hamburger =  new Hamburger (Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
-// // Добавка з майонезу
-// hamburger.addTopping(Hamburger.TOPPING_MAYO);
-// // Запитаємо скільки там калорій
-// console.log(“Calories: “ + hamburger.calculateCalories());
-// // скільки коштує
-// console.log("Price: “ + hamburger.calculatePrice());
-// // я тут передумав і вирішив додати ще приправу
-// hamburger.addTopping(Hamburger.TOPPING_SAUCE);
-// // А скільки тепер коштує?
-// console.log("Price with sauce: “ + hamburger.calculatePrice());
+
+// Студент 1:
+let studentOne = new Student("Serhiy", "Zhuravel", 1984);
+studentOne.addMark(100);
+studentOne.addMark(100);
+studentOne.present();
+studentOne.present();
+
+console.log(studentOne);
+console.log(studentOne.getAge());
+console.log(studentOne.summary());
+
+// Студент 2:
+let studentTwo = new Student("Oleksandr", "Bielikov", 1996);
+studentTwo.addMark(95);
+studentTwo.addMark(80);
+studentTwo.present();
+studentTwo.present();
+
+console.log(studentTwo);
+console.log(studentTwo.getAge());
+console.log(studentTwo.summary());
+
+// Студент 3:
+let studentThree = new Student("Elon", "Musk", 1971);
+studentThree.addMark(60);
+studentThree.addMark(60);
+studentThree.present();
+studentThree.absent();
+studentThree.absent();
+
+console.log(studentThree);
+console.log(studentThree.getAge());
+console.log(studentThree.summary());
