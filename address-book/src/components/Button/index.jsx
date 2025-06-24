546082d698841e6a343a5772ad73b10a287e74c5
Oleksandr-Bielikov <vergil.belikov96@gmail.com>
2025-06-24 20:24:31 +0300

    Delete address-book directory

--- address-book/src/components/Button/index.jsx
+++
@@ -1,7 +0,0 @@
-function Button({title, onClick, type = "button", className}) {
-    return (
-        <button className={className} type={type} onClick={onClick}>{title}</button>
-    )
-}
-
-export default Button;
--- address-book/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactsReducer from './slices/contactsSlice';
-
-export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-    },
-});
-
